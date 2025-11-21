7cac7cfb07001f6f2c368b0fa22e67af59d1f5b3
erwan-oss <132221094+erwan-oss@users.noreply.github.com>
2025-11-21 17:39:42 +0100

    Update app.py

--- app/app.py
+++ app/app.py
@@ -15,9 +15,9 @@ from utils import (
     simulate_scenarios,
     data_quality_report
 )
-# ===========================================================
+# ==========================================================
 # CONFIG STREAMLIT
-# ===========================================================
+# ==========================================================
 
 st.set_page_config(
     page_title="Marketing Cohortes & CLV",
@@ -103,6 +103,7 @@ elif returns_mode == "Neutraliser":
     st.markdown("🟦 *Retours neutralisés (CA = 0)*")
 
 st.markdown("---")
+
 
 
 
